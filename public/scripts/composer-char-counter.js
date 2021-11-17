f3e539f23375a658f41c4f6ac237f92279750243
jeff <jeffk713@gmail.com>
2021-11-16 16:09:47 -0800

    implement existing tweet and style it. also implement fixed nav bar

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,11 +8,11 @@ $(document).ready(function () {
       `${140 - textLength.length}`
     );
 
-    // change counter color accodring to the remaining counter
-    if (remainingCounter.val() < 0) {
+    // change counter color accodring to the remaining counter value
+    const counterValue = remainingCounter.val();
+    if (counterValue < 0) {
       $('.new-tweet-form-output').css({ color: 'red' });
-    }
-    if (remainingCounter.val() >= 0) {
+    } else {
       $('.new-tweet-form-output').css({ color: 'unset' });
     }
   });
