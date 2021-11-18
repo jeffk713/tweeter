7111229cf6eb61c6afbd182b854eb9a915c512a6
jeff <jeffk713@gmail.com>
2021-11-17 17:45:17 -0800

    implement escape function for security and change style on tweet button for pointer cusor.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,8 @@
-// Fake data taken from initial-tweets.json
-const mockUser = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac',
-    },
-    content: {
-      text: 'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-];
+const escape = function (str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 $(document).ready(function () {
   // create a new tweet
@@ -39,7 +19,7 @@ $(document).ready(function () {
         <span>${tweetData.user.handle}</span>
       </header>
       <main>
-        <p>${tweetData.content.text}</p>
+        <p>${escape(tweetData.content.text)}</p>
       </main>
       <footer>
         <span>${timeago.format(tweetData.created_at)}</span>
@@ -72,11 +52,31 @@ $(document).ready(function () {
   $('.new-tweet-form').on('submit', event => {
     event.preventDefault();
 
-    const text = $(event.target).serialize();
-    $.ajax({ type: 'POST', url: '/tweets', data: text }).then(res =>
-      loadTweets()
-    );
+    // when the MAX number of letters is exceeded
+    const remainingCounter = $('.counter').val();
+    if (remainingCounter < 0) {
+      return alert('tweet is too long');
+    }
+
+    const queryString = $(event.target).serialize();
+
+    //extract user input from queryString
+    const input = queryString.replace('text=', '');
+    // when input is empty string or null
+    if (!input) {
+      return alert('please input');
+    }
+
+    // create POST request
+    $.ajax({ type: 'POST', url: '/tweets', data: queryString })
+      .then(res => {
+        // clear input field and load tweets
+        $('#tweet-text').val('');
+        loadTweets();
+      })
+      .catch(error => alert(error.message));
   });
 
+  // laodTweets at initial render
   loadTweets();
 });
